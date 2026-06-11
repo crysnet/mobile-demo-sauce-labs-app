9278a2f20b543a0990960ff0455e99018333261e
Crysthel Aparicio <crysapa13@gmail.com>
2026-06-11 16:42:50 -0600

    test(e2e): add platform name to test title
    clean up unused commented test code in the e2e spec file

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,12 +1,7 @@
 import LoginPage from "../pageobjects/product.page.js";
 
 describe("My Login application", () => {
-  it("launch app and should assert products screen is visible", async () => {
+  it(`launch app and should assert products screen is visible - ${driver.capabilities.platformName}`, async () => {
     await LoginPage.assertProductsScreenIsVisible();
-
-    // await expect(SecurePage.flashAlert).toBeExisting();
-    // await expect(SecurePage.flashAlert).toHaveText(
-    //   expect.stringContaining("You logged into a secure area!"),
-    // );
   });
 });
